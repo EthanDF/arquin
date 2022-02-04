3cc687dc615b7a5304d38b92ad9535be7f0fb5dd
ethan.fenichel <ethan.fenichel@modmed.com>
2022-02-04 15:09:55 -0500

    new notebook to capture the unique subjects without having to use Sheets to do it. Please see https://pypi.org/project/gsheets/ for using the gsheets library and correct configuration

--- separate_subjets.py
+++ separate_subjets.py
@@ -1,1 +1,7 @@
 # this is to rewrite the code that separates the csv file into separate subjects
+
+pip
+install - -upgrade
+google - api - python - client
+google - auth - httplib2
+google - auth - oauthlib
